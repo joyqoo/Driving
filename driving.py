9bc1ae26b833b955005ab0929036c6d68c84f030
Joy Hu <www3391@gmail.com>
2020-05-19 16:37:37 +0800

    '新增台灣'

---
+++ driving.py
@@ -0,0 +1,15 @@
+country = input('請問你是哪國人?')
+age =input('請問年齡?')
+age =int(age)
+if country=='台灣' ||country=='Taiwan'
+	if age > 18
+		print('你可以考駕照')
+	else
+		print('你還不可以考駕照')	
+elif country=='美國' || country=='US'
+	if age > 18
+		print('你可以考駕照')
+	else
+		print('你還不可以考駕照')
+else
+	print('無法辨識此國家')	
